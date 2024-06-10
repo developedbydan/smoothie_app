eece16f8f1b5a222b8db16ac4eff9d76efee0178
Danijel <developedbydane@gmail.com>
2024-06-10 23:07:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-black min-h-screen">
+      <h1 className="text-white">App</h1>
+    </div>
+  );
+};
+
+export default App;
